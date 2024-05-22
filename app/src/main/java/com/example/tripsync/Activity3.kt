2d170aac673a66239ea7347b7a1ab8c589828151
aluisaoliveira <aluisaoliveira@ipvc.pt>
2024-05-22 15:38:55 +0100

    2

--- app/src/main/java/com/example/tripsync/Activity3.kt
+++ app/src/main/java/com/example/tripsync/Activity3.kt
@@ -1,6 +1,8 @@
 package com.example.tripsync
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity3 : AppCompatActivity() {
@@ -8,5 +10,11 @@ class Activity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.intro_slider_3)
+
+        val button3: Button = findViewById(R.id.Button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, Activity3::class.java)
+            startActivity(intent)
+        }
     }
 }
