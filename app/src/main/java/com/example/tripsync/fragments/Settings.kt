3f8d6f10e5607194cddf118dbd4fe34cf922c3ff
aluisaoliveira <aluisaoliveira@ipvc.pt>
2024-06-03 01:17:00 +0100

    set

---
+++ app/src/main/java/com/example/tripsync/fragments/Settings.kt
@@ -0,0 +1,48 @@
+package com.example.tripsync.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tripsync.Activity3
+
+class Settings: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {}
+        // Inflate the layout for this fragment
+       // return inflater.inflate(R.layout.fragment_settings, container, false)
+   // }
+   // override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+     //   super.onViewCreated(view, savedInstanceState)
+
+
+       // val perfilButton = view.findViewById<perfilButtton>(R.id.perfilButtton)
+       // val linguagemButton = view.findViewById<linguagemButtton>(R.id.linguagemButtton)
+        //val viagensButton = view.findViewById<viagensButtton>(R.id.viagensButtton)
+        //val offButton = view.findViewById<offButtton>(R.id.offButtton)
+
+        // Set click listeners
+    //    perfilButton.setOnClickListener {
+            // Handle perfilButton click
+      //  }
+
+        //     linguagemButton.setOnClickListener {
+
+            //        requireActivity().supportFragmentManager.beginTransaction()
+              //  .replace(R.id.fragment_container, Activity3())
+                //           .addToBackStack(null)
+        //        .commit()
+        //    }
+
+        //  viagensButton.setOnClickListener {
+            // Handle viagensButton click
+        //   }
+
+        //   offButton.setOnClickListener {
+            // Handle offButton click
+            // }
+   // }
